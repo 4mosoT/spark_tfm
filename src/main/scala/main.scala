206c7c8b1b62f1a7921e8c974abbbaaac541ba1d
marco <marco.a.formoso@gmail.com>
2017-07-01 22:58:49 +0200

    Custom Horizontal Partitioner

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -10,15 +10,15 @@ object main extends App {
   sc.setLogLevel("ERROR")
   val input = sc.textFile(args(0))
 
-  println(get_classes_and_count(input, ","))
 
+  val classes = get_classes_and_count(input, ",")
 
+  val num_partitions = 8
+  val partitioned = input.map(line => (line.split(",").last, line)).partitionBy(new HorizontalPartitioner(num_partitions, classes.keys.toSet))
 
-
-
-
-
-
-
-
+  for (x <- 0 until num_partitions) {
+    println("Partition " + x)
+    val partition = partitioned.mapPartitionsWithIndex((index, iter) => if (index == x) iter else Iterator())
+    classes.keys.foreach(x => println(x, partition.filter(_._1 == x).count()))
+  }
 }
