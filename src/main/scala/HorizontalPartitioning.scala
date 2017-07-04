7cf3c91eb46cc57ca826294d53e7ee347cfd772b
marco <marco.a.formoso@gmail.com>
2017-07-04 22:10:04 +0200

    Deleted useless function

--- src/main/scala/HorizontalPartitioning.scala
+++ src/main/scala/HorizontalPartitioning.scala
@@ -10,12 +10,16 @@ object HorizontalPartitioning {
 
   def main(args: Array[String]): Unit = {
 
+    //TODO: Parse arguments
+
     val ss = SparkSession.builder().appName("hsplit").master("local[*]").getOrCreate()
+
     val dataframe = ss.read.option("maxColumns", "30000").csv(args(0))
+
     val input = dataframe.rdd
-    val classes = get_classes_and_count(input)
 
     val numParts = 8
+
     val br_numParts = ss.sparkContext.broadcast(numParts)
     val partitioned = input.map(row => (row.get(row.length - 1), row)).groupByKey()
       .flatMap({
@@ -30,11 +34,5 @@ object HorizontalPartitioning {
     println(partitioned.countByKey())
   }
 
-
-  def get_classes_and_count(rdd: RDD[Row]): collection.Map[Any, Long] = {
-    //Class is last word of row
-    rdd.map(row => row.get(row.size - 1)).countByValue()
-
-  }
 
 }
