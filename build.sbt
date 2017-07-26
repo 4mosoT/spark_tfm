592b6e5d2897b00da701b3ef115130c97f828c73
marco <marco.a.formoso@gmail.com>
2017-07-26 19:30:28 +0200

    Selected features with calculated threshold done.

--- build.sbt
+++ build.sbt
@@ -5,13 +5,18 @@ version := "1.0"
 scalaVersion := "2.11.11"
 
 libraryDependencies ++= Seq(
-  "org.apache.spark" %% "spark-core" % "2.1.1",
-  "org.apache.spark" %% "spark-sql" % "2.1.1",
-  "org.apache.spark" %% "spark-mllib" % "2.1.1"
+  "org.apache.spark" %% "spark-core" % "2.2.0",
+  "org.apache.spark" %% "spark-sql" % "2.2.0",
+  "org.apache.spark" %% "spark-mllib" % "2.2.0"
 )
 
 
 libraryDependencies += "nz.ac.waikato.cms.weka" % "weka-stable" % "3.8.1"
+
+resolvers += "Spark Packages Repo" at "http://dl.bintray.com/spark-packages/maven"
+
+libraryDependencies += "saurfang" % "spark-knn" % "0.2.0"
+
 
 assemblyMergeStrategy in assembly := {
   case PathList("META-INF", xs @ _*) => MergeStrategy.discard
--- src/main/scala/HorizontalPartitioning.scala
+++ src/main/scala/HorizontalPartitioning.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.SparkContext
-import org.apache.spark.ml.Pipeline
-import org.apache.spark.ml.classification.{DecisionTreeClassifier, NaiveBayes}
+import org.apache.spark.ml.{Pipeline, PipelineStage}
+import org.apache.spark.ml.classification.{LinearSVC, OneVsRest, KNNClassifier}
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssembler}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
@@ -8,9 +8,7 @@ import weka.attributeSelection.{CfsSubsetEval, GreedyStepwise, InfoGainAttribute
 import weka.filters.Filter
 import weka.filters.supervised.attribute.AttributeSelection
 
-import scala.collection.immutable
 
-//https://github.com/saurfang/spark-knn
 //TODO: Merge with VerticalPartitioning
 
 object HorizontalPartitioning {
@@ -29,26 +27,26 @@ object HorizontalPartitioning {
 
     val dataframe = ss.read.option("maxColumns", "30000").csv(args(0))
 
-     val input = dataframe.rdd
+    val input = dataframe.rdd
     val numParts: Int = 10
 
-    /**
+    /** *****************************
       * Creation of attributes maps
-      */
+      * *****************************/
     val class_index = dataframe.columns.length - 1
     val first_row = dataframe.first().toSeq.map(_.toString)
     val inverse_attributes_ = collection.mutable.Map[String, Int]()
     val attributes = dataframe.columns.zipWithIndex.map({
-      case ( column_name, index) =>
-      // If categorical we need to add the distinct values it can take plus its column name
-      if (parseNumeric(first_row(index)).isEmpty || index == class_index) {
-        inverse_attributes_ += column_name -> index
-        index -> (Some(dataframe.select(dataframe.columns(index)).distinct().collect().toSeq.map(_.get(0).toString)), column_name)
-      } else {
-        // If not categorical we only need column name
-        inverse_attributes_ += column_name -> index
-        index -> (None, column_name)
-      }
+      case (column_name, index) =>
+        // If categorical we need to add the distinct values it can take plus its column name
+        if (parseNumeric(first_row(index)).isEmpty || index == class_index) {
+          inverse_attributes_ += column_name -> index
+          index -> (Some(dataframe.select(dataframe.columns(index)).distinct().collect().toSeq.map(_.get(0).toString)), column_name)
+        } else {
+          // If not categorical we only need column name
+          inverse_attributes_ += column_name -> index
+          index -> (None, column_name)
+        }
     }).toMap
 
 
@@ -61,9 +59,9 @@ object HorizontalPartitioning {
     //    println(fisherRatio(dataframe, br_attributes.value, ss.sparkContext))
     //    println(System.currentTimeMillis() - start_program_time)
 
-    /**
+    /** **************************
       * Getting the Votes vector.
-      */
+      * **************************/
 
     val partitioned = input.map(row => (row.get(row.length - 1), row)).groupByKey()
       .flatMap({
@@ -100,9 +98,9 @@ object HorizontalPartitioning {
 
     }.reduceByKey(_ + _).collect()
 
-    /**
+    /** ******************************************
       * Computing 'Selection Features Threshold'
-      */
+      * ******************************************/
 
     val avg_votes = votes.map(_._2).sum.toDouble / votes.length
     val std_votes = math.sqrt(votes.map(votes => math.pow(votes._2 - avg_votes, 2)).sum / votes.length)
@@ -110,47 +108,29 @@ object HorizontalPartitioning {
     val maxVote = (avg_votes + (std_votes / 2)).toInt
 
     //We get the features that aren't in the votes set. That means features -> Votes = 0
+    // **Class column included**
     val selected_features_0_votes = inverse_attributes.keySet.diff(votes.map(_._1).toSet)
+
+    val alpha = 0.75
+    var votes_errors = collection.mutable.ArrayBuffer[(Int, Double)]()
 
     for (a <- minVote to maxVote by 5) {
       // We add votes below Threshold value
       val selected_features = (selected_features_0_votes ++ votes.filter(_._2 < a).map(_._1)).toSeq
-      val df = dataframe.select(selected_features.head, selected_features.tail: _*)
+      val selected_features_dataframe = dataframe.select(selected_features.head, selected_features.tail: _*)
+      val retained_feat = (selected_features.length.toDouble / dataframe.columns.length) * 100
 
-      // We need to transform categorical data to one_hot to work with MLlib
-      //Creation of pipeline
-      var pipeline: Array[org.apache.spark.ml.PipelineStage] = Array()
-      var columns_to_assemble: Array[String] = Array()
-      df.columns.filter {
-        cname =>
-          val original_attr_index = inverse_attributes(cname)
-          attributes(original_attr_index)._1.isDefined && original_attr_index != class_index
-      }.foreach {
-        cname =>
-          val st_indexer = new StringIndexer().setInputCol(cname).setOutputCol(s"${cname}_index")
-          pipeline = pipeline :+ st_indexer
-          pipeline = pipeline :+ new OneHotEncoder().setInputCol(st_indexer.getOutputCol).setOutputCol(s"${cname}_vect")
-          columns_to_assemble = columns_to_assemble :+ s"${cname}_vect"
-      }
-
-      //Transform class column from categorical to index
-      pipeline = pipeline :+ new StringIndexer().setInputCol(attributes(class_index)._2).setOutputCol("label")
-      pipeline = pipeline :+  new  VectorAssembler().setInputCols(columns_to_assemble).setOutputCol("features")
-
-      val df_one_hot = new Pipeline().setStages(pipeline :+ new DecisionTreeClassifier()).fit(df).transform(df)
-      df_one_hot.show(
-
-      )
-      val evaluator = new MulticlassClassificationEvaluator()
-        .setLabelCol("label")
-        .setPredictionCol("prediction")
-        .setMetricName("precision")
-      val accuracy = evaluator.evaluate(df_one_hot)
-      println("Test Error = " + (1.0 - accuracy))
-
-
-
+      //We are using classification error as complexityMeasure
+      val classifier = new LinearSVC()
+      val ovsr = new OneVsRest().setClassifier(classifier)
+      val error = classification_error(selected_features_dataframe, attributes, inverse_attributes, class_index, ovsr)
+      votes_errors += ((a, alpha * error + (1 - alpha) * retained_feat))
     }
+
+
+    val selected_threshold = votes_errors.minBy(_._2)._1
+    val selected_features_threshold = (selected_features_0_votes ++ votes.filter(_._2 < selected_threshold).map(_._1)) - attributes(class_index)._2
+    print(selected_features_threshold)
 
 
     //      #For use with Weka library
@@ -160,6 +140,10 @@ object HorizontalPartitioning {
 
   }
 
+  /*********************
+    * Auxiliar Functions
+    *********************/
+
   def parseNumeric(s: String): Option[Double] = {
     try {
       Some(s.toDouble)
@@ -168,6 +152,58 @@ object HorizontalPartitioning {
 
     }
   }
+
+  def classification_error(selected_features_dataframe: DataFrame,
+                           attributes: Map[Int, (Option[Seq[String]], String)],
+                           inverse_attributes: Map[String, Int],
+                           class_index: Int,
+                           classifier: PipelineStage): Double = {
+
+    //Creation of pipeline
+    var pipeline: Array[PipelineStage] = Array()
+
+    //Mllib needs to special columns [features] and [label] to work. We have to assemble the columns we selected
+    var columns_to_assemble: Array[String] = selected_features_dataframe.columns.filter {
+      cname =>
+        val original_attr_index = inverse_attributes(cname)
+        attributes(original_attr_index)._1.isEmpty && original_attr_index != class_index
+    }
+
+    //Cast to double of columns that aren't categorical
+    val df = columns_to_assemble.foldLeft(selected_features_dataframe) { case (new_df, cname) =>
+      new_df.withColumn(cname, new_df(cname).cast("double"))
+    }
+
+    // Transform categorical data to one_hot to work with MLlib
+    df.columns.filter {
+      cname =>
+        val original_attr_index = inverse_attributes(cname)
+        attributes(original_attr_index)._1.isDefined && original_attr_index != class_index
+    }.foreach {
+      cname =>
+        val st_indexer = new StringIndexer().setInputCol(cname).setOutputCol(s"${cname}_index")
+        pipeline = pipeline :+ st_indexer
+        pipeline = pipeline :+ new OneHotEncoder().setInputCol(st_indexer.getOutputCol).setOutputCol(s"${cname}_vect")
+        columns_to_assemble = columns_to_assemble :+ s"${cname}_vect"
+    }
+
+    //Transform class column from categorical to index
+    pipeline = pipeline :+ new StringIndexer().setInputCol(attributes(class_index)._2).setOutputCol("label")
+    //Assemble features
+    pipeline = pipeline :+ new VectorAssembler().setInputCols(columns_to_assemble).setOutputCol("features")
+
+    //Since SVM are for binary classification we need to use the OneVsRest strategy
+    val df_one_hot = new Pipeline().setStages(pipeline :+ classifier).fit(df).transform(df)
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("label")
+      .setPredictionCol("prediction")
+      .setMetricName("accuracy")
+    1 - evaluator.evaluate(df_one_hot)
+
+
+  }
+
 
   def fisherRatio(dataframe: DataFrame, attributes: Map[Int, (Option[Seq[String]], String)], sc: SparkContext): Double = {
 
